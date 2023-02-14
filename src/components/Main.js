6febe77e81a13445dd69064e6d8082b936213fef
Ethan Storm <stormethan10@gmail.com>
2023-02-14 14:20:27 -0800

    added filter feature

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,23 +9,38 @@ import Button from 'react-bootstrap/Button'
 class Main extends React.Component {
 	constructor(props) {
 		super(props)
-		this.state = { theme: 1 }
+
+		this.state = { theme: true, filter: false }
 	}
 
 	changeTheme = () => {
 		this.setState({ theme: !this.state.theme })
 	}
 
+	changeFilter = () => {
+		this.setState({ filter: !this.state.filter })
+	}
+
+	filterBeasts = beasts => {
+		if (!this.state.filter) return this.props.beasts
+		let sorted = []
+		this.props.beasts.forEach(beast => {
+			sorted.push(beast)
+		})
+		return sorted.sort((a, b) => b.horns - a.horns)
+	}
+
 	render() {
 		return (
 			<Container fluid className='main'>
-				<div className='themebutton'>
+				<div className='inteactionbuttons'>
 					<Button onClick={this.changeTheme}>Change Theme!</Button>
+					<Button onClick={this.changeFilter}>Sort!</Button>
 				</div>
 
 				<div className='beastsdisplay'>
 					{this.state.theme
-						? this.props.beasts?.map((beast, i) => {
+						? this.filterBeasts().map((beast, i) => {
 								return <HornedBeast key={i} beast={beast} />
 						  })
 						: this.props.beasts.map((beast, i) => {
